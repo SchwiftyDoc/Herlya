dac213fc926ec49a7b5f4f13ec5ad4a23315ad7b
SchwiftyDoc <corentin_dek@hotmail.com>
2017-02-16 18:52:54 +0100

    Tileset class implementation

--- public/js/main.js
+++ public/js/main.js
@@ -1,16 +1,26 @@
 /**
  * Created by Coreuh on 12-02-17.
  */
-let image = document.getElementById('image')
+let configuration = include('config.json')
+console.log(configuration.canvasId)
+
+let canvas = document.getElementById('gameView')
+let ctx = canvas.getContext('2d')
+let image = new Image()
+image.src = "/assets/svg/perso.svg"
+
+// Test draw char
+let character = ctx.drawImage(image, 0, 0, 100, 200)
+
+// Test draw map
+let map = new Map("premiere");
+
 window.onload = function() {
-    var canvas = document.getElementById('canvas')
-    var ctx = canvas.getContext('2d')
+  var canvas = document.getElementById('canvas');
+  var ctx = canvas.getContext('2d');
 
-    ctx.fillStyle = 'blue'
-    ctx.fillRect(10, 10, 100, 50)
+  canvas.width  = map.getLargeur() * 32;
+  canvas.height = map.getHauteur() * 32;
 
-    ctx.strokeStyle = 'red'
-    ctx.strokeRect(75, 75, 50, 50)
-
-    ctx.drawImage(image, 0, 0, 146.2, 419.2)
+  map.dessinerMap(ctx);
 }
