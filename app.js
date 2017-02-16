dcfb39b3cec09c534073d67b33639aa470372699
SchwiftyDoc <corentin_dek@hotmail.com>
2017-02-16 02:10:49 +0100

    Github Hook implement test 5

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ let http = require('http')
 let server = http.createServer()
 let url = require('url')
 
-// Test 7 pour le GithubHook avec un console log
+// Test 8 pour le GithubHook avec un console log
 console.log('GitHub Hook!')
 
 server.on('request', (request, response) => {
