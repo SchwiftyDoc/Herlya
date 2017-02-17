d6d860a88f9a0dcd43157fbeb9d792106cc07865
SchwiftyDoc <corentin_dek@hotmail.com>
2017-02-17 11:21:32 +0100

    Variable portability problem in $.getJSON() (map.js)

---
+++ public/js/classes/map.js
@@ -0,0 +1,27 @@
+Map = function (name) {
+  let mapData
+  $.getJSON('/assets/maps/' + name + '.json', function (data) {
+    mapData = data
+    this.tileset = new Tileset(data.tileset)
+    this.terrain = data.terrain
+  })
+}
+
+Map.prototype.getWidth = function() {
+  return this.terrain[0].length
+}
+
+Map.prototype.getHeight = function() {
+  return this.terrain.length
+}
+
+Map.prototype.draw = function(context) {
+  for(var i = 0, l = this.terrain.length ; i < l ; i++) {
+    var ligne = this.terrain[i];
+    var y = i * 32;
+    for(var j = 0, k = ligne.length ; j < k ; j++) {
+      this.tileset.drawTile(ligne[j], context, j * 32, y);
+    }
+  }
+}
+
